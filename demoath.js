9b66ab912d637c7e4584af11b66d5d1b98bfefc5
unknown <apailwan53@gmail.com>
2026-08-16 15:07:03 +0530

    Demo file from Atharva tester

---
+++ demoath.js
@@ -0,0 +1,8 @@
+console.log("Hello, this is a test script for Saucedemo!");
+consople.log("This script will perform some basic actions on the Saucedemo website.");
+console.log("We will be testing the login functionality, adding items to the cart, and checking out.");
+console.log("Let's get started with the test cases!");
+console.log("First, we will navigate to the Saucedemo website and verify that the login page is displayed correctly.");
+console.log("Next, we will enter valid credentials and log in to the application.");
+console.log("After logging in, we will add a few items to the shopping cart and proceed to checkout.");
+console.log("Finally, we will complete the checkout process and verify that the order confirmation page is displayed.");
